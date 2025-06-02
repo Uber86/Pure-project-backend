28096ad84b84018ce2f8faca1be863b15bc3ed0c
Сергей <lockup86@rambler.ru>
2025-06-02 21:45:11 +0300

    ТЗ написать бэк часть приложения для знакомств pure

---
+++ src/test/java/com/example/pure/PureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
